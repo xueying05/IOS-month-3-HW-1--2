d13cf21d664a99165937a9aad7645d7650618c42
Nuraiym <1991958945@qq.com>
2022-11-21 18:52:18 +0600

    Initial Commit

---
+++ IOS month 3 HW 1 -2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS month 3 HW 1 -2
+//
+//  Created by NURAIYM KUDAIAR KYZY on 21/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
